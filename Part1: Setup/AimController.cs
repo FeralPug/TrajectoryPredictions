4ad0d5f0aba4dc639c548ef805a26a95ffc76a00
FeralPug <72169728+FeralPug@users.noreply.github.com>
2022-02-05 22:12:09 -0500

    Added scripts seperately

---
+++ Part1: Setup/AimController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+    public Vector2 rotSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+
+        Vector3 desRot = new Vector3();
+        desRot.x = Input.GetAxis("Vertical") * rotSpeed.x;
+        desRot.y = Input.GetAxis("Horizontal") * rotSpeed.y;
+
+        desRot *= Time.deltaTime;
+
+        currentRotation += desRot;
+
+        transform.rotation = Quaternion.Euler(currentRotation);
+    }
+}
---
+++ Part1: Setup/LaunchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchController : MonoBehaviour
+{
+    public float launchVelocity = 15f;
+    public ProjectileController projectilePrefab;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LaunchProjectile();
+        }
+    }
+
+    void LaunchProjectile()
+    {
+        ProjectileController projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+        projectile.Rigidbody.velocity = transform.forward * launchVelocity;
+    }
+}
+
